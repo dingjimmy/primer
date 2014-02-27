c2ea028655865d29f041c2e37ec626b08c8f0793
ding_jimmy <ding_jimmy@hotmail.com>
2014-02-27 13:44:47 +0000

    Add smart-property initialiser classes; these will be the basis of fluent property-initialisation-api

--- Primer/SmartProperties/IDataProperty.cs
+++ Primer/SmartProperties/IDataProperty.cs
@@ -5,8 +5,8 @@ namespace Primer.SmartProperties
     interface IDataProperty<T>
     {
         bool IsReadOnly { get; set; }
-        string Name { get; set; }
-        T Value { get; set; }
-        ViewModel ViewModel { get; set; }
+        string Name { get; }
+        T Data { get; set; }
+        ViewModel ViewModel { get; }
     }
 }
