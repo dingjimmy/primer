1728370285026676342a38005f8b56d0c18487ad
ding_jimmy <ding_jimmy@hotmail.com>
2014-11-05 16:12:36 +0000

    Create messaging interfaces.

---
+++ Primer/IMessage.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) James Dingle
+
+using System;
+
+namespace Primer
+{
+
+    /// <summary>
+    /// A communication that is sent between two or more ViewModels.
+    /// </summary>
+    public interface IMessage
+    {
+
+
+        /// <summary>
+        /// The class from which the message originated from; usually a ViewModel.
+        /// </summary>
+        Object Sender { get; set; }
+
+
+        /// <summary>
+        /// The time and date that the message was broadcast.
+        /// </summary>
+        DateTime BroadcastOn { get; set; }
+
+
+    }
+}
