97ced2780e70be357b83b7510718141d7fcd8fea
ding_jimmy <ding_jimmy@hotmail.com>
2014-11-05 16:13:37 +0000

    Implement basic Messages: MessageBase and PropertyChangedMessage.

---
+++ Primer/MessageBase.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) James Dingle
+
+using System;
+
+namespace Primer
+{
+
+    /// <summary>
+    /// A communication that is sent between two or more ViewModels.
+    /// </summary>
+    public class MessageBase : IMessage
+    {
+
+
+        /// <summary>
+        /// The class from which the message originated from; usually a ViewModel.
+        /// </summary>
+        public Object Sender { get; set; }
+
+
+
+        /// <summary>
+        /// The time and date that the message was broadcast.
+        /// </summary>
+        public DateTime BroadcastOn { get; set; }
+
+
+
+        // Primary Constructor
+        public MessageBase()
+        {
+            BroadcastOn = DateTime.Now;
+        }
+
+
+    }
+}
---
+++ Primer/PropertyChangedMessage.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) James Dingle
+
+using System;
+
+namespace Primer
+{
+
+    /// <summary>
+    /// A communication that indicates that a ViewModel or Facade property has been changed.
+    /// </summary>
+    public class PropertyChangedMessage : MessageBase
+    {
+
+        /// <summary>
+        /// The name of the Property that has changed.
+        /// </summary>
+        public string Name { get; set; }
+
+    }
+
+}
