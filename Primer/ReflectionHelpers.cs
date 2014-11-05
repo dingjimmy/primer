a3d50e88ca7e5fd3e6f960ef468f57ede7506568
ding_jimmy <ding_jimmy@hotmail.com>
2014-11-05 16:04:45 +0000

    Create basic reflection helper.

---
+++ Primer/ReflectionHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Primer
+{
+    public static class Reflection
+    {
+
+        public static string GetPropertyName<T>(Expression<Func<T>> propertyToInspect)
+        {
+            return ((MemberExpression)propertyToInspect.Body).Member.Name;
+        }
+
+
+        public static string GetMethodName<T>(Expression<Func<T>> methodToInspect)
+        {
+            return ((MethodCallExpression)methodToInspect.Body).Method.Name;
+        }
+
+
+        public static string GetMethodName(Expression<Action> methodToInspect)
+        {
+            return ((MethodCallExpression)methodToInspect.Body).Method.Name;
+        }
+
+    }
+}
