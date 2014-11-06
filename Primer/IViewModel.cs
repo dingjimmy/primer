88fc2997f607744a7b48af859483f6de55951b12
ding_jimmy <ding_jimmy@hotmail.com>
2014-11-06 12:32:06 +0000

    Create extension method for simple Lookup creation.

---
+++ Primer/IViewModel.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) James Dingle
+
+using System;
+
+namespace Primer
+{
+    public interface IViewModel
+    {
+        IMessagingChannel Channel;
+        IValidator Validator;
+    }
+}
---
+++ Primer/ViewModelHelpers.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) James Dingle
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace Primer.Helpers
+{
+    public static class ViewModelHelpers
+    {
+
+        /// <summary>
+        /// A helper that creates a Lookup to be exposed by the <see cref="Primer.ViewModel"/>, based upon a collection of <see cref="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="System.Type"/> of the source items.</typeparam>
+        /// <param name="viewModel">The <see cref="Primer.ViewModel"/> the new lookup will be exposed by.</param>
+        /// <param name="sourceItems">The collection source</param>
+        /// <param name="keyExpression">A lamda expression (or annonymous function) that specifies the source item property to use as the lookup key.</param>
+        /// <param name="descriptionExpression">A lamda expression (or annonymous function) that specifies the source item property to use a the lookup description.</param>
+        /// <returns>An instance of <see cref="Primer.ILookup<T>"/>; That is a collection of <see cref="ILookupItem<T>"/> that can be displayed by a View.</returns>
+        public static ILookup<T> CreateLookup<T>(this ViewModelBase viewModel, IEnumerable<T> sourceItems, Func<T, string> keyExpression, Func<T, string> descriptionExpression)
+        {
+
+            // init lookup collection
+            ILookup<T> lookup = new Lookup<T>();
+
+
+            // create a lookup-item for each source-item
+            foreach (var sourceItem in sourceItems)
+            {             
+                lookup.Add(keyExpression(sourceItem), sourceItem , descriptionExpression(sourceItem));
+            }
+
+            // return completed lookup to caller
+            return lookup;
+
+        }
+
+
+        public static ICommand CreateCommand(this ViewModelBase viewModel, Action actionToExecute)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public static ICommand CreateCommand<T>(this ViewModelBase viewModel, Action<T> actionToExecute)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
+
