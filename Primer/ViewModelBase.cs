a52d5bff3d7478cb26102cdf75eddaadefe2866f
ding_jimmy <ding_jimmy@hotmail.com>
2014-12-04 23:20:27 +0000

    Clean-up ViewModelBase: make it StyleCop compliant.

--- Primer/ViewModelBase.cs
+++ Primer/ViewModelBase.cs
@@ -1,177 +1,122 @@
-﻿// Copyright (c) James Dingle
-
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq.Expressions;
+﻿//-----------------------------------------------------------------------
+// <copyright file="ViewModelBase.cs" company="James Dingle">
+//     Copyright (c) James Dingle
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Primer
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq.Expressions;
 
     /// <summary>
     /// Represents the base class for all Primer ViewModels.
     /// </summary>
     public abstract class ViewModelBase : PropertyChangedBase, IViewModel
     {
-
-
-        #region State Flags
-
-
-
-        private bool _IsLoaded = false;
-
+        private bool isLoaded = false;
+        private string displayName = string.Empty;
+    
         /// <summary>
-        /// Gets or sets a value that indicates whether the ViewModel has been initialised successfully.
+        /// Gets or sets a value indicating whether the ViewModel has been initialized successfully.
         /// </summary>
-        /// <returns>
-        /// True if ViewModel initialisation has been successfull; otherwise false.
-        /// </returns>
         public bool IsLoaded
         {
-            get { return _IsLoaded; }
+            get 
+            { 
+                return this.isLoaded; 
+            }
+
             set
             {
-                SetProperty(() => IsLoaded, ref _IsLoaded, value);
+                this.SetProperty(() => this.IsLoaded, ref this.isLoaded, value);
             }
-        }
-
-
-
-
-        private string _DisplayName;
+        }     
 
         /// <summary>
         /// Gets or sets the display name of the ViewModel; Its main purpose is to be used as the text that appears on the title-bar of a window.
         /// </summary>
         public string DisplayName
         {
-            get { return _DisplayName; }
+            get 
+            { 
+                return this.displayName; 
+            }
+
             set
             {
-                SetProperty(() => DisplayName, ref _DisplayName, value);
+                this.SetProperty(() => this.DisplayName, ref this.displayName, value);
             }
         }
-        
 
+        /// <summary>
+        /// Gets or sets the default validator for this ViewModel.
+        /// </summary>
+        public IValidator Validator { get; set; }
 
-        #endregion
+        /// <summary>
+        /// Gets or sets the default messaging channel for this ViewModel.
+        /// </summary>
+        public IMessagingChannel Channel { get; set; }
 
-
-        #region Updating
-
+        /// <summary>
+        /// Gets or sets the default error logger for this ViewModel.
+        /// </summary>
+        public ILogger Logger { get; set; }
 
         /// <summary>
         /// Compares the current and proposed values; If they are not equal the current value is replaced with the proposed the <see cref="INotifyPropertyChanged.PropertyChanged"/>
         /// event is raised and <see cref="Primer.PropertyChangedMessage"/> message is broadcast.
         /// </summary>
+        /// <typeparam name="T">The type of value we are setting.</typeparam>
         /// <param name="propertyToSet">An expression which identifies the property to update.</param>
         /// <param name="currentValue">The current value of the property.</param>
         /// <param name="proposedValue">The proposed value of the property</param>
         public void SetProperty<T>(Expression<Func<T>> propertyToSet, ref T currentValue, T proposedValue)
         {
-            SetProperty(propertyToSet, ref currentValue, proposedValue, false, true);
+            this.SetProperty(propertyToSet, ref currentValue, proposedValue, false, true);
         }
-
-
-
+        
         /// <summary>
         /// Compares the current and proposed values; If they are not equal the current value is replaced with the proposed the <see cref="INotifyPropertyChanged.PropertyChanged"/>
         /// event is raised and <see cref="Primer.PropertyChangedMessage"/> message is broadcast.
         /// </summary>
+        /// <typeparam name="T">The type of value we are setting.</typeparam>
         /// <param name="propertyToSet">An expression which identifies the property to update.</param>
         /// <param name="currentValue">The current value of the property.</param>
         /// <param name="proposedValue">The proposed value of the property</param>
         /// <param name="forceUpdate">Force the property to update, regardless of if the proposed and current values are the same.</param>
         public void SetProperty<T>(Expression<Func<T>> propertyToSet, ref T currentValue, T proposedValue, bool forceUpdate)
         {
-            SetProperty(propertyToSet, ref currentValue, proposedValue, forceUpdate, true);
+            this.SetProperty(propertyToSet, ref currentValue, proposedValue, forceUpdate, true);
         }
-
-
 
         /// <summary>
         /// Compares the current and proposed values; If they are not equal the current value is replaced with the proposed the <see cref="INotifyPropertyChanged.PropertyChanged"/>
         /// event is raised and <see cref="Primer.PropertyChangedMessage"/> message is broadcast.
         /// </summary>
+        /// <typeparam name="T">The type of value we are setting.</typeparam>
         /// <param name="propertyToSet">An expression which identifies the property to update.</param>
         /// <param name="currentValue">The current value of the property.</param>
         /// <param name="proposedValue">The proposed value of the property</param>
         /// <param name="forceUpdate">Force the property to update, regardless of if the proposed and current values are the same.</param>
-        /// <param name="broadcastMessage">Choose whether to broadcast a PropertyChanged message on the ViewModel messaging channel.</param>
+        /// <param name="broadcastMessage">Choose whether to broadcast a PropertyChanged message on the ViewModel's messaging channel.</param>
         public void SetProperty<T>(Expression<Func<T>> propertyToSet, ref T currentValue, T proposedValue, bool forceUpdate, bool broadcastMessage)
         {
             var propertyName = Reflection.GetPropertyName(propertyToSet);
 
             if (forceUpdate || !EqualityComparer<T>.Default.Equals(currentValue, proposedValue))
             {
-
                 currentValue = proposedValue;
-                RaisePropertyChanged(propertyName);
+                this.RaisePropertyChanged(propertyName);
 
-                if (broadcastMessage && _Channel != null)
-                    _Channel.Broadcast(new PropertyChangedMessage() { Name = propertyName, Sender = this });
-
+                if (broadcastMessage && this.Channel != null)
+                {
+                    this.Channel.Broadcast(new PropertyChangedMessage() { Name = propertyName, Sender = this });
+                }
             }
         }
-
-
-        #endregion
-
-
-        #region Validation
-
-
-        private IValidator _Validator;
-
-        /// <summary>
-        /// Gets or sets the default validator for this ViewModel.
-        /// </summary>
-        public IValidator Validator
-        { 
-            get { return _Validator; }
-            protected set { _Validator = value; }
-        }
-
-
-        #endregion
-
-
-        #region Messaging
-        
-
-        private IMessagingChannel _Channel;
-
-        /// <summary>
-        /// Gets or sets the default messaging channel for this ViewModel.
-        /// </summary>
-        public IMessagingChannel Channel
-        {
-            get { return _Channel; }
-            protected set { _Channel = value; }
-        }
-
-
-
-        #endregion
-
-
-        #region Logging
-
-
-        private ILogger _Logger;
-
-        /// <summary>
-        /// Gets or sets the default error logger for this ViewModel.
-        /// </summary>
-        public ILogger Logger
-        {
-            get { return _Logger; }
-            protected set { _Logger = value; }
-        }
-
-
-        #endregion
-
     }
 }
